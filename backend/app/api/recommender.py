d8aa1b2de8dacb1a31b19a06034208184e9af2cf
Muhammad Anang Mahrub <muhanangmahrub@gmail.com>
2025-12-28 22:09:22 +0700

    update recommender.py

--- backend/app/api/recommender.py
+++ backend/app/api/recommender.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from app.services.embedding import embed_user_persona
 from app.services.faiss_store import faiss_store
+from app.services.classifier import predict_job_function, vectorizer, selector, model
 
 router = APIRouter()
 @router.post("/recommendations/")
@@ -8,10 +9,22 @@ async def get_recommendations(user_persona: str, top_k: int = 10):
     """
     Get job recommendations based on user persona
     """
+    # Predict job function using classifier
+
+    job_function_labels, job_function_probs = predict_job_function([user_persona], vectorizer, selector, model)
+
     # Step 1: Embed the user persona
     query_embedding = embed_user_persona([user_persona])
 
     # Step 2: Search for similar jobs in Faiss store
     recommendations = faiss_store.search(query_embedding, k=top_k)
 
-    return {"recommendations": recommendations}
+    return {
+        "recommendations": {
+            "faiss": recommendations,
+            "predicted_job_field": {
+                "labels": job_function_labels,
+                "probabilities": job_function_probs.tolist()
+            }
+        }
+    }
---
+++ backend/app/services/classifier.py
@@ -0,0 +1,30 @@
+import joblib
+from tensorflow.keras import models
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_DIR = os.path.join(BASE_DIR, "..", "models")
+
+artifacts = joblib.load(os.path.join(MODEL_DIR, "text_preprocessor.joblib"))
+vectorizer = artifacts["vectorizer"]
+selector = artifacts["selector"]
+label_map = {0: 'Creative & Media',
+ 1: 'Education',
+ 2: 'Engineering',
+ 3: 'Finance & Accounting',
+ 4: 'HR & Administration',
+ 5: 'Hospitality & Service',
+ 6: 'IT & Software',
+ 7: 'Manufacturing & Operations',
+ 8: 'Other',
+ 9: 'Sales & Marketing'}
+
+model = models.load_model(os.path.join(MODEL_DIR, "mlp_model.keras"))
+
+def predict_job_function(texts, vectorizer, selector, model):
+    X = vectorizer.transform(texts)
+    X = selector.transform(X).toarray()
+    probs = model.predict(X)
+    labels = probs.argmax(axis=1)
+    labels = label_map[int(labels[0])]
+    return labels, probs
